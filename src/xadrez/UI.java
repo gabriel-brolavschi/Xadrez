51b37da26881eaee9b35a546bb66ba3f22d92fe1
gabriel-brolavschi <gabrielbroslavschidacosta@gmail.com>
2020-07-17 03:07:26 -0300

    FINALIZADO

--- src/xadrez/UI.java
+++ src/xadrez/UI.java
@@ -39,11 +39,11 @@ public class UI {
     public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
     public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
     public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
-    
-    public static void clearScreen(){
+
+    public static void clearScreen() {
         System.out.print("\033[H\033[2J");
         System.out.flush();
-        
+
     }
 
     public static ChessPosition readChessPosition(Scanner sc) {
@@ -69,16 +69,24 @@ public class UI {
         System.out.println("  a b c d e f g h");
 
     }
-    
-    public static void printMatch(ChessMatch chessMatch, List<ChessPiece> captured){
+
+    public static void printMatch(ChessMatch chessMatch, List<ChessPiece> captured) {
         printBoard(chessMatch.getPieces());
         System.out.println();
         printCapturedPieces(captured);
         System.out.println();
         System.out.println("TURN: " + chessMatch.getTurn());
-        System.out.println("WAITING PLAYER: " + chessMatch.getCurrentPlayer());
+        if (!chessMatch.getCheckMate()) {
+            System.out.println("WAITING PLAYER: " + chessMatch.getCurrentPlayer());
+            if (chessMatch.getCheck()) {
+                System.out.println("******** CHECK! ********");
+            }
+        } else {
+            System.out.println("******** CHECKMATE! ********");
+            System.out.println("WINNER " + chessMatch.getCurrentPlayer());
+        }
     }
-    
+
     public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves) {
         for (int i = 0; i < pieces.length; i++) {
             System.out.print((8 - i) + " ");
@@ -92,8 +100,8 @@ public class UI {
     }
 
     private static void printPiece(ChessPiece piece, boolean background) {
-        
-        if(background){
+
+        if (background) {
             System.out.print(ANSI_BLUE_BACKGROUND);
         }
         if (piece == null) {
@@ -107,8 +115,8 @@ public class UI {
         }
         System.out.print(" ");
     }
-    
-    private static void printCapturedPieces(List<ChessPiece> captured){
+
+    private static void printCapturedPieces(List<ChessPiece> captured) {
         List<ChessPiece> white = captured.stream().filter(x -> x.getColor() == Color.WHITE).collect(Collectors.toList());
         List<ChessPiece> black = captured.stream().filter(x -> x.getColor() == Color.BLACK).collect(Collectors.toList());
         System.out.println("Captured pieces: ");
@@ -120,7 +128,7 @@ public class UI {
         System.out.print(ANSI_YELLOW);
         System.out.print(Arrays.toString(black.toArray()));
         System.out.println(ANSI_RESET);
-    
+
     }
 
 }
--- src/xadrez/Xadrez.java
+++ src/xadrez/Xadrez.java
@@ -9,6 +9,7 @@ import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
+import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.List;
@@ -21,37 +22,49 @@ public class Xadrez {
         ChessMatch chessMatch = new ChessMatch();
         List<ChessPiece> captured = new ArrayList<>();
 
-        while (true) {
-            try{
-            UI.clearScreen();
-            UI.printMatch(chessMatch, captured);
-            System.out.println();
-            System.out.print("SOURCE: ");
-            ChessPosition source = UI.readChessPosition(sc);
-            
-            boolean [][] possibleMoves = chessMatch.possibleMoves(source);
-            UI.clearScreen();
-            UI.printBoard(chessMatch.getPieces(), possibleMoves);
-            
-            System.out.println();
-            System.out.print("TARGET: ");
-            ChessPosition target = UI.readChessPosition(sc);
+        while (!chessMatch.getCheckMate()) {
+            try {
+                UI.clearScreen();
+                UI.printMatch(chessMatch, captured);
+                System.out.println();
+                System.out.print("SOURCE: ");
+                ChessPosition source = UI.readChessPosition(sc);
 
-            ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
-            
-            if(capturedPiece != null){
-                captured.add(capturedPiece);
+                boolean[][] possibleMoves = chessMatch.possibleMoves(source);
+                UI.clearScreen();
+                UI.printBoard(chessMatch.getPieces(), possibleMoves);
+
+                System.out.println();
+                System.out.print("TARGET: ");
+                ChessPosition target = UI.readChessPosition(sc);
+
+                ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+
+                if (capturedPiece != null) {
+                    captured.add(capturedPiece);
+                }
+
+                if (chessMatch.getPromoted() != null) {
+                    System.out.print("ENTER PIECE FOR PROMOTION (B/N/R/Q): ");
+                    String type = sc.nextLine().toUpperCase();
+                    while(!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")) {
+                    System.out.print("INVALID VALUE!!  ENTER PIECE FOR PROMOTION (B/N/R/Q): ");
+                    type = sc.nextLine().toUpperCase();
+                    }
+                    chessMatch.replacePromotedPiece(type);
+                }
+            } catch (ChessException e) {
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            } catch (InputMismatchException e) {
+                System.out.println(e.getMessage());
+                sc.nextLine();
+
             }
-        }catch(ChessException e){
-                System.out.println(e.getMessage());
-                sc.nextLine();
-        }catch(InputMismatchException e){
-                System.out.println(e.getMessage());
-                sc.nextLine();
-            
+
         }
+        UI.clearScreen();
+        UI.printMatch(chessMatch, captured);
 
     }
-
-}
 }
